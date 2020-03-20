275d54b48103e36e328c29124c1cf46e7d8b6b19
Devin James <devsch1@gmail.com>
2020-03-19 20:30:54 -0500

    the first three videos

---
+++ OpenGLPractice/Source.cpp
@@ -0,0 +1,52 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>;
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    
+    if (glewInit() != GLEW_OK) {
+        std::cout << "error!\n";
+    }
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /*draw a triangle*/
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-.5f, -.5f);
+        glVertex2f(0.0f, .5f);
+        glVertex2f(.5f, -.5f);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
