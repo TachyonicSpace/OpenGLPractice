3bacdbe35098d8692995b0f66bae31f16ce2bb16
Devin James <devsch1@gmail.com>
2020-03-20 13:26:32 -0500

    making the shaders

--- OpenGLPractice/Source.cpp
+++ OpenGLPractice/Source.cpp
@@ -3,6 +3,34 @@
 
 #include <iostream>;
 
+static unsigned int CompileShader(unsigned int type, const std::string source) {
+    unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+    const char* src = source.c_str;
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    
+    //todo:error handling
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return(program);
+    //14:06
+}
+
 int main(void)
 {
     GLFWwindow* window;
