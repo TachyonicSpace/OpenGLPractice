71725f1b0329f79009a90652df68a80280d2d323
Devin James <devsch1@gmail.com>
2020-03-19 21:13:40 -0500

    just dealt with the buffers, will not draw a triangle because we do not have any shaders

--- OpenGLPractice/Source.cpp
+++ OpenGLPractice/Source.cpp
@@ -29,6 +29,17 @@ int main(void)
         std::cout << "error!\n";
     }
 
+    float positions[] = {
+        -.5f, -.5f,
+        0.0f, 0.5f,
+        .5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -36,10 +47,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         /*draw a triangle*/
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-.5f, -.5f);
-        //glVertex2f(0.0f, .5f);
-        glVertex2f(.5f, -.5f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
