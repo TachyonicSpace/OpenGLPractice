9b390e98ca0b83b71c0aa8880947b0109a04178d
Devin James <devsch1@gmail.com>
2020-03-23 21:01:56 -0500

    commenting code

--- OpenGLPractice/Source.cpp
+++ OpenGLPractice/Source.cpp
@@ -25,22 +25,32 @@ int main(void)
     glfwMakeContextCurrent(window);
     
     
+    //makes sure that glew starts correctly
     if (glewInit() != GLEW_OK) {
         std::cout << "error!\n";
     }
 
+    //our data
     float positions[] = {
         -.5f, -.5f,
         0.0f, 0.5f,
         .5f, -0.5f
     };
 
+    //enables us to astore our data onto the gpu
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    //enables us to specify how we want to interprite our data
     glEnableVertexAttribArray(0);
+    /*
+    how many objects are in the vector
+    object type
+    do we need to normilize the data
+    how big is the vector
+    */
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(positions[0]) * 2, 0);
 
     /* Loop until the user closes the window */
