11ce15670d78b6c78482940bde734cccccd866e6
Devin James <devsch1@gmail.com>
2020-03-23 22:09:12 -0500

    commenting the code

--- OpenGLPractice/Source.cpp
+++ OpenGLPractice/Source.cpp
@@ -6,11 +6,13 @@
 #include <string>
 #include <sstream>
 
+//a struct to allow us to return multiple return types
 struct ShaderProgramSource {
     std::string VertexSource;
     std::string FragmentSource;
 };
 
+//allows us to parse in shader from files instread of string literals
 static ShaderProgramSource parseShader(const std::string& filepath) {
     std::ifstream stream(filepath);
 
@@ -36,14 +38,20 @@ static ShaderProgramSource parseShader(const std::string& filepath) {
     }return { ss[0].str(), ss[1].str() };
 }
 
+/*compile a shader (gpu program) from a string*/
 static unsigned int CompileShader(unsigned int type, const std::string source) {
+    //creates a shader of specified type
     unsigned int id = glCreateShader(type);
+    //stores the source code as a string
     const char* src = source.c_str();
+    //load source code into openGL and compiles it
     glShaderSource(id, 1, &src, nullptr);
     glCompileShader(id);
 
+    //makes sure code compiled correctly
     int result;
     glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    //if compile error, print our the error
     if (result == GL_FALSE) {
         int length;
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
@@ -53,20 +61,24 @@ static unsigned int CompileShader(unsigned int type, const std::string source) {
         glDeleteShader(id);
         return 0;
     }
-
+    //else return compiled code location
     return id;
 }
 
+//create the shaders from strings
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    //makes a new gpu program, and both shaders compiled code
     unsigned int program = glCreateProgram();
     unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
     unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
+    //attatch shaders to program and makes sure it is valid
     glAttachShader(program, vs);
     glAttachShader(program, fs);
     glLinkProgram(program);
     glValidateProgram(program);
 
+    //delete the shaders since they are incorporated into the program and return program
     glDeleteShader(vs);
     glDeleteShader(fs);
 
@@ -81,7 +93,7 @@ int main()
     if (!glfwInit())
         return -1;
 
-    
+
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -93,12 +105,14 @@ int main()
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    
-    
+
+
+    //makes sure glew initilizes correctly
     if (glewInit() != GLEW_OK) {
         std::cout << "error!\n";
     }
 
+    //an array holding the positions of the  triangle plus one for the square
     float positions[] = {
         -.5f, -.5f,
         0.5f, -0.5f,
@@ -106,27 +120,42 @@ int main()
         -.5f, .5f,
     };
 
+    //openGL how to read the positions to make a square out of two triangles
     unsigned int indicies[] = {
         0, 1, 2,
         2, 3, 0
     };
 
+    //makes the buffer that will hold the positons
     unsigned int buffer;
     glGenBuffers(1, &buffer);
+    //binds the buffer to the current scope
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    //specifies the type, how big the array is in bytes, points to the data, and hints at openGL how the data will be used
     glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    //specifies how to traverse the array
     glEnableVertexAttribArray(0);
+    /*starting index in data
+    how many points in each vertex
+    data type
+    do we need to normilize the data or not
+    how far in memory do we move bevore hitting the next vertex
+    a pointer*/
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(positions[0]) * 2, 0);
 
     unsigned int ibo;//indexBufferObject
     glGenBuffers(1, &ibo);
+    //binds it the the gpu scope
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    //adds the index buffer to the gpu
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indicies, GL_STATIC_DRAW);
 
+    //makes some shader code from the file at this location
     ShaderProgramSource source = parseShader("res/shaders/Basic.shader");
-
+    //compiles the shader and returns the location on the gpu
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+    //tells openGL to use this shader
     glUseProgram(shader);
 
     /* Loop until the user closes the window */
